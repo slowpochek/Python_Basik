8c86685b01f64a0eadc1b1b2dc1479fe71ff6064
Slowpochek <aniskindanil467@gamil.com>
2026-03-03 22:28:47 +0200

    Commit HM1

---
+++ HomeWork/Lesson2.py
@@ -0,0 +1,49 @@
+# 1. Квадрат числа.
+
+# user_number= int(input("Enter your number: "))
+# square_number=user_number**2
+# print(f"Square of {user_number} will be: {square_number}")
+
+# 2. “Середнє трьох чисел”
+
+# first_number= float(input("Enter your number: "))
+# second_number= float(input("Enter your number: "))
+# third_number= float(input("Enter your number: "))
+# arithmetic_mean=(first_number+second_number+third_number)/3
+# print(f"The average arithmetic one will be: {arithmetic_mean} ")
+
+# 3. “Перетворення хвилин у години”
+
+# user_number=int(input("Enter the number of minutes"))
+# hours=user_number//60
+# minutes=user_number%60
+# print(f"It will be {hours} hours and {minutes} minutes")
+
+# 4. “Розрахунок знижки”
+
+# price=float(input("Enter the price of the product: "))
+# discount = int(input("Enter the discount percentage: "))
+# discounted_price=price-(price/100*discount)
+# print(f"The discounted price of the product is: {discounted_price}")
+
+# 5. “Остання цифра числа”
+
+# user_number=int(input("enter your number"))
+# last_number=user_number % 10
+# print(f"Last digit of the number {user_number} is {last_number} ")
+
+# 6. “Периметр прямокутника”
+
+# length=float(input("Enter the length of the side of the rectangle: "))
+# width=float(input("Enter the width of the side of the rectangle: "))
+# perimeter=(length*2)+(width*2)
+# print(f"The perimeter of the rectangle is {perimeter}")
+
+# 7. Виведення числа в стовпчик
+
+# user_number=int(input("Enter your number"))
+# thousands=user_number//1000
+# hundreds=user_number%1000//100
+# tens=user_number%100//10
+# units=user_number%10
+# print(f"your number consists of \n {thousands} \n {hundreds} \n {tens} \n {units}")
