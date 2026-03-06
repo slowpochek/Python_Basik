4b79bed565f2d1ce3837473931122c5c82ab7939
Slowpochek <aniskindanil467@gamil.com>
2026-03-06 22:17:13 +0200

    Commit HM2

---
+++ HomeWork/Lessons3.py
@@ -0,0 +1,44 @@
+# Калькулятор
+
+# Первый вариант
+
+# first_number= int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+# print("Select an action : + , - , * , / ")
+# action = input()
+# match action:
+#     case "+":
+#         print(first_number + second_number)
+#     case"-":
+#         print(first_number - second_number)
+#     case "/":
+#         if second_number == 0:
+#             print("You can't divide by zero")
+#         else:
+#             print(first_number / second_number)
+#     case "*":
+#         print(first_number * second_number)
+#     case _:
+#         print("an corect action")
+
+# второй вариант
+
+# first_number= int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+# print("Select an action : + , - , * , / ")
+# action = input()
+# if action == "+":
+#     print(first_number + second_number)
+# elif action == "-":
+#     print(first_number - second_number)
+# elif action == "*":
+#     print(first_number * second_number)
+# elif action == "/":
+#     if second_number == 0:
+#         print("You can't divide by zero")
+#     else:
+#         print(first_number / second_number)
+# else:
+#     print("an correct action")
+
+
