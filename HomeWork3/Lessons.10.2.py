358546ce6c852f632fcc5528f39206919e4c1e34
Slowpochek <aniskindanil467@gamil.com>
2026-04-14 00:49:46 +0300

    Commit HM10

---
+++ HomeWork3/Lessons.10.2.py
@@ -0,0 +1,16 @@
+import re
+def first_word(text):
+
+    word= re.search(r'\w+(\'\w+)?', text)
+    word = word.group()
+    return word
+
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
